dd2683963876e4d6fa399f11c5161d010339a29e
LuigiVessella <vessellaluigi12@gmail.com>
2017-02-01 18:15:12 +0100

    spazi

--- Address.c
+++ Address.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
 int main(){
+
 float num = 50.55;
 float *p_num = &num;
+
 printf("L'indirizzo in memoria e': %p\n",p_num);
+
 return 0;
 }
--- Cast.c
+++ Cast.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
 
 int main(){
+
   double x_double;
   int x_int;
+
   printf("Inserisci un numero a virgola mobile: ");
   scanf("%lf", &x_double);
   x_int=(int)x_double;
   printf("La parte intera e' %d\n", x_int);
   printf("Il numero completo e' %f\n", x_double);
+
 return 0;
 }
