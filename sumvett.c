81cd286cc1205d3d8fbf02996475f087d46453ec
LuigiVessella <vessellaluigi12@gmail.com>
2017-02-02 19:55:19 +0100

    file

---
+++ sumvett.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(){
+  int vett[2];
+  int somma;
+  float media;
+
+  printf("Inserisci tre valori interi:\n");
+  printf("?");
+  scanf("%d", &vett[0]);
+  printf("?");
+  scanf("%d", &vett[1]);
+  printf("?");
+  scanf("%d",&vett[2]);
+  
+  printf("Numeri inseriti:\n");
+  printf("%d\n%d\n%d\n", vett[0],vett[1],vett[2]);
+
+  somma = vett[0]+vett[1]+vett[2];
+  media = somma/3;
+
+  printf("Somma:%d\nMedia:%.1f\n",somma,media);
+
+  return 0;
+
+}
