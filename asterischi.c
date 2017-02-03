16657b6e8334cffb60c6851f3285eea6c8d5a5c1
LuigiVessella <vessellaluigi12@gmail.com>
2017-02-03 19:36:00 +0100

    file

---
+++ asterischi.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<time.h>
+
+int main(){
+    struct timespec intervallo;
+    intervallo.tv_sec = 0;
+    intervallo.tv_nsec = 700*1000*1000;
+
+    printf("*\n");
+    nanosleep(&intervallo, NULL);
+    
+    printf("*\n");
+    nanosleep(&intervallo, NULL);	
+
+    printf("*\n");
+    nanosleep(&intervallo, NULL);
+   
+    return 0;
+
+}
